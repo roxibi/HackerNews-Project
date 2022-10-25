a4b76c58374dcb4380f8eaca8c7dbd263d531f12
roxibi <106161599+roxibi@users.noreply.github.com>
2022-10-25 18:23:33 +0200

    importing json data:titles in main

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-import data from './data.json'
+
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
 function App() {
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
      <Header />
+     {hits.map((hit)=> <h3>{hit.title}</h3>)}
      <Main />
      <Footer />
 
--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import Article from './Article'
+import data from './data.json'
 
 function Main() {
-
-
+    const {hits}=data;
+const article=()=> {hits.map((hit)=> <h3>{hit.title}</h3>)}
   return (
     <div className='main'>
         <p>Main </p>
     
-    <Article />
-    <Article />
-    <Article />
-    <Article />
-    <Article />
+       
     
     </div>
   )
