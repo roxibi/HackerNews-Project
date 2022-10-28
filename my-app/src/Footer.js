cf872b908ff809081855098ac8954ccd732fc693
Shafia Shamsu <shafia.sks@gmail.com>
2022-10-28 11:16:15 +0200

    hide footer

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div> Guidelines  | FAQ |  contact us  </div>
+    // <div className='footer-class'> Guidelines  | FAQ |  contact us  </div>
+    <div></div>
   )
 }
 
